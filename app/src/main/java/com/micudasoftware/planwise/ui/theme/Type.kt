e102dcb3268442e85f0ef1f8152dbe3b8de09c74
Dušan Mičuda <micudasoftware@gmail.com>
2024-07-18 12:28:56 +0200

    💄 Add fonts

--- app/src/main/java/com/micudasoftware/planwise/ui/theme/Type.kt
+++ app/src/main/java/com/micudasoftware/planwise/ui/theme/Type.kt
@@ -2,27 +2,51 @@ package com.micudasoftware.planwise.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.micudasoftware.planwise.R
+
+private  val fontFamilyInter = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.inter_black, FontWeight.Black),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_extra_light, FontWeight.ExtraLight),
+    Font(R.font.inter_thin, FontWeight.Thin),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    bodyMedium = TextStyle(
+        fontFamily = fontFamilyInter,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
     ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamilyInter,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 16.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = fontFamilyInter,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = fontFamilyInter,
+        fontWeight = FontWeight.Bold,
+        fontSize = 11.sp,
+        lineHeight = 14.sp,
+    ),
+    /* Other default text styles to override
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
