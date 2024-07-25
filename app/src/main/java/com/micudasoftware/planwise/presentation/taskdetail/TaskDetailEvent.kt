dc94516a6164925ba50ee82dc32cb78a95cfaecb
Dušan Mičuda <micudasoftware@gmail.com>
2024-07-25 16:47:18 +0200

    💄 Add TaskDetailScreen

---
+++ app/src/main/java/com/micudasoftware/planwise/presentation/taskdetail/TaskDetailEvent.kt
@@ -0,0 +1,7 @@
+package com.micudasoftware.planwise.presentation.taskdetail
+
+import com.micudasoftware.planwise.presentation.common.UIEvent
+
+sealed class TaskDetailEvent: UIEvent {
+
+}
---
+++ app/src/main/java/com/micudasoftware/planwise/presentation/taskdetail/components/model/CategoryModel.kt
@@ -0,0 +1,14 @@
+package com.micudasoftware.planwise.presentation.taskdetail.components.model
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Model for a category of task.
+ *
+ * @param name The name of the category.
+ * @param color The color of the category.
+ */
+data class CategoryModel(
+    val name: String,
+    val color: Color,
+)
