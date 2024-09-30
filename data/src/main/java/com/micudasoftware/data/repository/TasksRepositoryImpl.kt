198b70dbc90f3a2b95d55511da5f51a994b5c169
Dušan Mičuda <micudasoftware@gmail.com>
2024-09-30 19:36:30 +0200

    🐛 Fix database access in main thread

--- data/src/main/java/com/micudasoftware/data/repository/TasksRepositoryImpl.kt
+++ data/src/main/java/com/micudasoftware/data/repository/TasksRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.micudasoftware.data.repository
 import com.micudasoftware.data.database.TasksDatabase
 import com.micudasoftware.data.repository.model.Task
 import com.micudasoftware.data.repository.model.TaskCategory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.time.LocalDate
 
 /**
@@ -17,24 +19,31 @@ internal class TasksRepositoryImpl(
     private val taskDao = tasksDatabase.tasksDao()
     private val taskCategoriesDao = tasksDatabase.taskCategoriesDao()
 
-    override suspend fun saveTask(task: Task) =
+    override suspend fun saveTask(task: Task) = withContext(Dispatchers.IO) {
         taskDao.insertTask(task.toEntity())
+    }
 
-    override suspend fun getTasksForDay(day: LocalDate): List<Task> =
+    override suspend fun getTasksForDay(day: LocalDate): List<Task> = withContext(Dispatchers.IO) {
         taskDao.getTasksForDay(day.toEpochDay()).map { Task.fromEntity(it) }
+    }
 
-    override suspend fun getTaskById(taskId: Long): Task? =
+    override suspend fun getTaskById(taskId: Long): Task? = withContext(Dispatchers.IO) {
         taskDao.getTaskById(taskId)?.let { Task.fromEntity(it) }
+    }
 
-    override suspend fun deleteTask(task: Task) =
+    override suspend fun deleteTask(task: Task) = withContext(Dispatchers.IO) {
         taskDao.deleteTask(task.toEntity())
+    }
 
-    override suspend fun saveTaskCategory(taskCategory: TaskCategory) =
+    override suspend fun saveTaskCategory(taskCategory: TaskCategory) = withContext(Dispatchers.IO) {
         taskCategoriesDao.insertTaskCategory(taskCategory.toEntity())
+    }
 
-    override suspend fun getTaskCategories(): List<TaskCategory> =
+    override suspend fun getTaskCategories(): List<TaskCategory> = withContext(Dispatchers.IO) {
         taskCategoriesDao.getAllTaskCategories().map { TaskCategory.fromEntity(it) }
+    }
 
-    override suspend fun deleteTaskCategory(taskCategory: TaskCategory) =
+    override suspend fun deleteTaskCategory(taskCategory: TaskCategory) = withContext(Dispatchers.IO) {
         taskCategoriesDao.deleteTaskCategory(taskCategory.toEntity())
+    }
 }
